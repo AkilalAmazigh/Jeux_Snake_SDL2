5a847c8c4aeae08a125c1325e14d93842672286b
AkilalAmazigh <amazighakilal@gmail.com>
2022-03-19 21:22:43 +0100

    changement structure du prog

--- src/main.c
+++ src/main.c
@@ -2,19 +2,44 @@
 #include <stdlib.h>
 #include "../include/SDL.h"
 
+//Declaration de variables globale
+
 SDL_Window* fenetre;
 SDL_Renderer* rendu;
+SDL_Rect carre;
+SDL_Rect pomme;
+
+//signature des fonctions
+
+void creation_fenetre();
+void creation_carre();
+void creation_pomme();
+
+//Programme principal
 
 int main(int argc, char *argv[])
 {
-    SDL_Rect carre;
-    SDL_Rect pomme;
     SDL_Init(SDL_INIT_EVERYTHING);
+    creation_fenetre();
+    creation_carre();
+    creation_pomme();
+    SDL_RenderPresent(rendu); // Pour afficher le rendu
+    SDL_Delay(3000); // Pour afficher la fenetre pdt 3000 ms
+    SDL_DestroyRenderer(rendu);
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
+    return 0;
+}   
 
+//Fonctions du jeu
+
+void creation_fenetre()
+{
     fenetre = SDL_CreateWindow("fenetre", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_CENTERED, 1100, 800, SDL_WINDOW_SHOWN );
     rendu = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
-   
-    // creation d'un carre pour le serpent en couleur
+}
+void creation_carre()// creation d'un carre pour le serpent en couleur
+{
     SDL_SetRenderDrawColor(rendu, 66, 206, 245, SDL_ALPHA_OPAQUE);
     carre.h = 45;
     carre.w = 45; 
@@ -22,8 +47,9 @@ int main(int argc, char *argv[])
     carre.y = 400;
     SDL_RenderDrawRect(rendu, &carre); // Pour dessiner le contour
     SDL_RenderFillRect(rendu, &carre); // Pour remplir l'interieur du carre
-
-    // creation d'un carre pour la pomme en couleur
+}
+void creation_pomme()// creation d'un carre pour la pomme en couleur
+{
     SDL_SetRenderDrawColor(rendu, 229, 10, 10, SDL_ALPHA_OPAQUE);
     pomme.h = 45;
     pomme.w = 45; 
@@ -31,11 +57,4 @@ int main(int argc, char *argv[])
     pomme.y = 0;
     SDL_RenderDrawRect(rendu, &pomme); // Pour dessiner le contour
     SDL_RenderFillRect(rendu, &pomme); // Pour remplir l'interieur du carre
-
-    SDL_RenderPresent(rendu); // Pour afficher le rendu
-    SDL_Delay(3000); // Pour afficher la fenetre pdt 3000 ms
-    SDL_DestroyRenderer(rendu);
-    SDL_DestroyWindow(fenetre);
-    SDL_Quit();
-    return 0;
-    
+}
